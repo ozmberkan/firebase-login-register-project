00bc9501b9c70b7f080ad5794e9ebe95f878cb20
ozmberkan <ozmberkan@gmail.com>
2024-07-03 14:33:02 +0300

    new design

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import { auth } from '../firebase'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { Link, useNavigate } from 'react-router-dom'
 import toast, { Toaster } from 'react-hot-toast'
+import Banner from '../assets/Banner.svg'
 
 export default function Login() {
 
@@ -16,9 +17,9 @@ export default function Login() {
         await signInWithEmailAndPassword(auth, email, password);
         toast.success("Giriş Yapıldı", {
             id: "loginSuccess",
-            duration: 2000, // 2 saniye boyunca göster
+            duration: 2000,
             position: 'top-right',
-            onClose: () => navigate('/') // toast kapandığında yönlendir
+            onClose: () => navigate('/') 
         });
         } catch {
         toast.error("Hata Mevcut, bilgileri kontrol edin.");
@@ -26,12 +27,16 @@ export default function Login() {
     };
 
   return (
-      <div className='w-full h-screen flex justify-center items-center'>
-    <form className='flex flex-col gap-y-5' onSubmit={loginHandle}>
-        <input type="text" className='border px-5 py-2 rounded-md' placeholder='e-mail' value={email} onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" className='border px-5 py-2 rounded-md' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button type='submit' className='w-full hover:bg-violet-500 hover:text-white mx-auto border mt-6 border-violet-600 text-black rounded-md px-5 py-2 '>Log In</button>
-        <Link to="/register" type='submit' className='w-full flex justify-center items-center hover:bg-emerald-500 hover:text-white mx-auto border mt-6 border-emerald-600 text-black rounded-md px-5 py-2 '>Register</Link>
+      <div className='w-full h-screen flex flex-col justify-center items-center container mx-auto'>
+    <h1 className="my-6 font-black text-3xl  flex w-1/2 justify-start items-center text-violet-500">Giriş Yap</h1>
+    <form className='flex flex-col gap-y-5 bg-violet-600  ring-2 ring-offset-2 z-0 overflow-hidden ring-violet-500 w-1/2 rounded-xl p-12 h-[500px] relative' onSubmit={loginHandle}>
+        <img src={Banner} className=' absolute inset-0 -z-10 opacity-25' />
+        <input type="text" className=' border px-5 py-3 bg-white rounded-md' placeholder='e-mail' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" className=' border px-5 py-3 bg-white  rounded-md' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <div className='flex gap-x-5'>
+            <button type='submit' className='w-full h-14 border bg-white text-violet-500 font-bold rounded-md hover:ring-2 ring-offset-2 ring-violet-400 transition-all duration-500'>Giriş Yap</button>
+            <Link to="/register" type='submit' className='flex justify-center items-center w-full h-14 border bg-white text-violet-500 font-bold rounded-md hover:ring-2 ring-offset-2 ring-violet-400 transition-all duration-500'>Kayıt Ol </Link>
+        </div>
     </form>      
       </div>
   )
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,6 +3,7 @@ import { auth } from '../firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { Link } from 'react-router-dom'
 import toast, { Toaster } from 'react-hot-toast';
+import Banner from '../assets/Banner.svg'
 
 
 export default function Register() {
@@ -21,14 +22,17 @@ export default function Register() {
 };
 
   return (
-<div className='w-full h-screen flex justify-center items-center'>
-    <form className='flex flex-col gap-y-5' onSubmit={registerHandle}>
-        <input type="text" className='border px-5 py-2 rounded-md' placeholder='e-mail' value={email} onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" className='border px-5 py-2 rounded-md' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button type='submit' className='w-full hover:bg-violet-500 hover:text-white mx-auto border mt-6 border-violet-600 text-black rounded-md px-5 py-2 '>Register</button>
-        <Toaster  position="top-center"/>
-        <Link to="/login" type='submit' className='w-full flex justify-center items-center hover:bg-emerald-500 hover:text-white mx-auto border mt-6 border-emerald-600 text-black rounded-md px-5 py-2 '>Log In</Link>
+    <div className='w-full h-screen flex flex-col justify-center items-center container mx-auto'>
+    <h1 className="my-6 font-black text-3xl  flex w-1/2 justify-start items-center text-violet-500">Kayıt Ol</h1>
+    <form className='flex flex-col gap-y-5 bg-violet-600  ring-2 ring-offset-2 z-0 overflow-hidden ring-violet-500 w-1/2 rounded-xl p-12 h-[500px] relative' onSubmit={registerHandle}>
+        <img src={Banner} className=' absolute inset-0 -z-10 opacity-25' />
+        <input type="text" className=' border px-5 py-3 bg-white rounded-md' placeholder='e-posta giriniz..' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" className=' border px-5 py-3 bg-white  rounded-md' placeholder='parola giriniz..' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <div className='flex flex-col gap-y-5'>
+            <button type='submit' className='w-full h-14 border bg-white text-violet-500 font-bold rounded-md hover:ring-2 ring-offset-2 ring-violet-400 transition-all duration-500'>Kayıt Ol</button>
+            <Link to="/login" type='submit' className='flex justify-center items-center w-full h-14 border bg-white text-violet-500 font-bold rounded-md hover:ring-2 ring-offset-2 ring-violet-400 transition-all duration-500'>Hesabın var mı ? Giriş Yap</Link>
+        </div>
     </form>      
-</div>
+      </div>
   )
 }
